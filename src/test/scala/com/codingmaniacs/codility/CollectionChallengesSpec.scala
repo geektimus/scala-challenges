3238f51c9471c99a52532c1148f4c60426e9a88a
Geektimus <alexander.kno@gmail.com>
2018-03-02 12:02:16 -0500

    Fix comparison on the text to avoid confusions with exception messages

--- src/test/scala/com/codingmaniacs/codility/CollectionChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/codility/CollectionChallengesSpec.scala
@@ -8,48 +8,48 @@ class CollectionChallengesSpec extends FlatSpec with Matchers {
     val numbers = Array[Int]()
     val expected = 1
     val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(expected == missingInteger)
+    assert(missingInteger == expected)
   }
 
   "An array containing only [2]" should "contain [1] as missing integer" in {
     val numbers = Array(2)
     val expected = 1
     val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(expected == missingInteger)
+    assert(missingInteger == expected)
   }
 
   "An array containing only [1]" should "contain [2] as missing integer" in {
     val numbers = Array(1)
     val expected = 2
     val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(expected == missingInteger)
+    assert(missingInteger == expected)
   }
 
   "An array containing [1, 3, 6, 4, 1, 2]" should "contain [5] as a missing int" in {
     val numbers = Array(1, 3, 6, 4, 1, 2)
     val expected = 5
     val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(expected == missingInteger)
+    assert(missingInteger == expected)
   }
 
   "An array containing [4, 1, 5, 6, 2]" should "contain [3] as a missing int" in {
     val numbers = Array(4, 1, 5, 6, 2)
     val expected = 3
     val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(expected == missingInteger)
+    assert(missingInteger == expected)
   }
 
   "An array containing [1, 2, 3]" should "contain [4] as a missing int" in {
     val numbers = Array(1, 2, 3)
     val expected = 4
     val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(expected == missingInteger)
+    assert(missingInteger == expected)
   }
 
   "An array containing [-1,-3]" should "contain [1] as a missing int" in {
-    val numbers = Array(-1,-3)
+    val numbers = Array(-1, -3)
     val expected = 1
     val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(expected == missingInteger)
+    assert(missingInteger == expected)
   }
 }
