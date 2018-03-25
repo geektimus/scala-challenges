9f21392e95fa7ad4ac255164e933eb3a7ad287b9
Geektimus <alexander.kno@gmail.com>
2018-03-25 11:53:10 -0500

    Fix the order or the comparision inside the asserts

--- src/test/scala/com/codingmaniacs/others/StringChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/others/StringChallengesSpec.scala
@@ -7,34 +7,34 @@ class StringChallengesSpec extends FlatSpec with Matchers {
     val s = ""
     val expected = ""
     val reverse = StringChallenges.flip(s)
-    assert(expected == reverse)
+    assert(reverse == expected)
   }
 
   "The reverse of a string with one char" should "be the same char" in {
     val s = "a"
     val expected = "a"
     val reverse = StringChallenges.flip(s)
-    assert(expected == reverse)
+    assert(reverse == expected)
   }
 
   "The reverse of 'ab'" should "be 'ba'" in {
     val s = "ab"
     val expected = "ba"
     val reverse = StringChallenges.flip(s)
-    assert(expected == reverse)
+    assert(reverse == expected)
   }
 
   "The reverse of 'aaa'" should "be 'aaa'" in {
     val s = "aaa"
     val expected = "aaa"
     val reverse = StringChallenges.flip(s)
-    assert(expected == reverse)
+    assert(reverse == expected)
   }
 
   "The reverse of 'longer and complex words'" should " be 'sdrow xelpmoc dna regnol'" in {
     val s = "longer and complex words"
     val expected = "sdrow xelpmoc dna regnol"
     val reverse = StringChallenges.flip(s)
-    assert(expected == reverse)
+    assert(reverse == expected)
   }
 }
