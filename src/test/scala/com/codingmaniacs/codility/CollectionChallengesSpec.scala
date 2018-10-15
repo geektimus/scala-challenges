10cd1f533c178c3327b003235c401716475cfba8
Geektimus <alexander.kno+github@gmail.com>
2018-10-15 18:34:21 +0200

    Updated tests to use Specification and remove ScalaTest deps

--- src/test/scala/com/codingmaniacs/codility/CollectionChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/codility/CollectionChallengesSpec.scala
@@ -1,125 +1,131 @@
 package com.codingmaniacs.codility
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.specs2.mutable.Specification
 
-class CollectionChallengesSpec extends FlatSpec with Matchers {
+class CollectionChallengesSpec extends Specification {
 
-  "An empty array" should "contain [1] as missing integer" in {
-    val numbers = Array[Int]()
-    val expected = 1
-    val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(missingInteger == expected)
+  "The find missing integer function" should {
+    "show that [1] is a missing integer on an empty array" in {
+      val numbers = Array[Int]()
+      val expected = 1
+      val missingInteger = CollectionChallenges.findMissingInt(numbers)
+      missingInteger mustEqual expected
+    }
+
+    "show that [1] is a missing integer on [2]" in {
+      val numbers = Array(2)
+      val expected = 1
+      val missingInteger = CollectionChallenges.findMissingInt(numbers)
+      missingInteger mustEqual expected
+    }
+
+    "show that [2] is a missing integer on [1]" in {
+      val numbers = Array(1)
+      val expected = 2
+      val missingInteger = CollectionChallenges.findMissingInt(numbers)
+      missingInteger mustEqual expected
+    }
+
+    "show that [5] is a missing integer on [1, 3, 6, 4, 1, 2]" in {
+      val numbers = Array(1, 3, 6, 4, 1, 2)
+      val expected = 5
+      val missingInteger = CollectionChallenges.findMissingInt(numbers)
+      missingInteger mustEqual expected
+    }
+
+    "show that [3] is a missing integer on [4, 1, 5, 6, 2]" in {
+      val numbers = Array(4, 1, 5, 6, 2)
+      val expected = 3
+      val missingInteger = CollectionChallenges.findMissingInt(numbers)
+      missingInteger mustEqual expected
+    }
+
+    "show that [4] is a missing integer on [1, 2, 3]" in {
+      val numbers = Array(1, 2, 3)
+      val expected = 4
+      val missingInteger = CollectionChallenges.findMissingInt(numbers)
+      missingInteger mustEqual expected
+    }
+
+    "show that [1] is a missing integer on [-1,-3]" in {
+      val numbers = Array(-1, -3)
+      val expected = 1
+      val missingInteger = CollectionChallenges.findMissingInt(numbers)
+      missingInteger mustEqual expected
+    }
   }
 
-  "An array containing only [2]" should "contain [1] as missing integer" in {
-    val numbers = Array(2)
-    val expected = 1
-    val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(missingInteger == expected)
+  "The find unpaired element function" should {
+    "return the same element if the array contains only one element" in {
+      val numbers = Array(3)
+      val expected = 3
+      val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
+      unpairedNumber mustEqual expected
+    }
+
+    "return 1 as the unpaired element on [1,2,2]" in {
+      val numbers = Array(1, 2, 2)
+      val expected = 1
+      val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
+      unpairedNumber mustEqual expected
+    }
+
+    "return 3 as the unpaired element on [1,1,3,4,2,2,4]" in {
+      val numbers = Array(1, 1, 3, 4, 2, 2, 4)
+      val expected = 3
+      val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
+      unpairedNumber mustEqual expected
+    }
+
+    "return 5 as the unpaired element on [4,4,2,2,5,1,1,1,1,4,4]" in {
+      val numbers = Array(4, 4, 2, 2, 5, 1, 1, 1, 1, 4, 4)
+      val expected = 5
+      val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
+      unpairedNumber mustEqual expected
+    }
+
+    "return 5 as the unpaired element on [4,3,2,1,5,1,2,3,4]" in {
+      val numbers = Array(4, 3, 2, 1, 5, 1, 2, 3, 4)
+      val expected = 5
+      val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
+      unpairedNumber mustEqual expected
+    }
   }
 
-  "An array containing only [1]" should "contain [2] as missing integer" in {
-    val numbers = Array(1)
-    val expected = 2
-    val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(missingInteger == expected)
-  }
+  "To find the tape equilibrium the function" should {
+    "return 0 as the equilibrium on the tape [1,1]" in {
+      val numbers = Array(1, 1)
+      val expected = 0
+      val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
+      equilibrium mustEqual expected
+    }
 
-  "An array containing [1, 3, 6, 4, 1, 2]" should "contain [5] as a missing int" in {
-    val numbers = Array(1, 3, 6, 4, 1, 2)
-    val expected = 5
-    val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(missingInteger == expected)
-  }
+    "return 4 as the equilibrium on the tape [2,6]" in {
+      val numbers = Array(2, 6)
+      val expected = 4
+      val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
+      equilibrium mustEqual expected
+    }
 
-  "An array containing [4, 1, 5, 6, 2]" should "contain [3] as a missing int" in {
-    val numbers = Array(4, 1, 5, 6, 2)
-    val expected = 3
-    val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(missingInteger == expected)
-  }
+    "return 0 as the equilibrium on the tape [55,10,45]" in {
+      val numbers = Array(55, 10, 45)
+      val expected = 0
+      val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
+      equilibrium mustEqual expected
+    }
 
-  "An array containing [1, 2, 3]" should "contain [4] as a missing int" in {
-    val numbers = Array(1, 2, 3)
-    val expected = 4
-    val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(missingInteger == expected)
-  }
+    "return 1 as the equilibrium on the tape [3,1,2,4,3]" in {
+      val numbers = Array(3, 1, 2, 4, 3)
+      val expected = 1
+      val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
+      equilibrium mustEqual expected
+    }
 
-  "An array containing [-1,-3]" should "contain [1] as a missing int" in {
-    val numbers = Array(-1, -3)
-    val expected = 1
-    val missingInteger = CollectionChallenges.findMissingInt(numbers)
-    assert(missingInteger == expected)
-  }
-
-  "On an array containing one element, that element" should "be the unpaired one" in {
-    val numbers = Array(3)
-    val expected = 3
-    val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
-    assert(unpairedNumber == expected)
-  }
-
-  "On an array containing three elements [1,2,2], the unpaired element" should "be [1]" in {
-    val numbers = Array(1, 2, 2)
-    val expected = 1
-    val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
-    assert(unpairedNumber == expected)
-  }
-
-  "On an array containing seven elements [1,1,3,4,2,2,4], the unpaired element" should "be [3]" in {
-    val numbers = Array(1, 1, 3, 4, 2, 2, 4)
-    val expected = 3
-    val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
-    assert(unpairedNumber == expected)
-  }
-
-  "On an array containing eleven elements [4,4,2,2,5,1,1,1,1,4,4], the unpaired element" should "be [5]" in {
-    val numbers = Array(4, 4, 2, 2, 5, 1, 1, 1, 1, 4, 4)
-    val expected = 5
-    val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
-    assert(unpairedNumber == expected)
-  }
-
-  "On an array containing nine elements [4,3,2,1,5,1,2,3,4], the unpaired element" should "be [5]" in {
-    val numbers = Array(4, 3, 2, 1, 5, 1, 2, 3, 4)
-    val expected = 5
-    val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
-    assert(unpairedNumber == expected)
-  }
-
-  "On an array containing [1,1], the minimum difference" should "be 0" in {
-    val numbers = Array(1, 1)
-    val expected = 0
-    val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
-    assert(equilibrium == expected)
-  }
-
-  "On an array containing [2,6], the minimum difference" should "be 4" in {
-    val numbers = Array(2, 6)
-    val expected = 4
-    val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
-    assert(equilibrium == expected)
-  }
-
-  "On an array containing [55,10,45], the minimum difference" should "be 0" in {
-    val numbers = Array(55, 10, 45)
-    val expected = 0
-    val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
-    assert(equilibrium == expected)
-  }
-
-  "On an array containing [3,1,2,4,3], the minimum difference" should "be 1" in {
-    val numbers = Array(3, 1, 2, 4, 3)
-    val expected = 1
-    val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
-    assert(equilibrium == expected)
-  }
-
-  "On an array containing [-1000, 1000], the minimum difference" should "be 2000" in {
-    val numbers = Array(-1000, 1000)
-    val expected = 2000
-    val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
-    assert(equilibrium == expected)
+    "return 2000 as the equilibrium on the tape [-1000,1000]" in {
+      val numbers = Array(-1000, 1000)
+      val expected = 2000
+      val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
+      equilibrium mustEqual expected
+    }
   }
 }
--- src/test/scala/com/codingmaniacs/codility/NumericChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/codility/NumericChallengesSpec.scala
@@ -1,44 +1,49 @@
 package com.codingmaniacs.codility
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.specs2.mutable.Specification
 
-class NumericChallengesSpec extends FlatSpec with Matchers {
-  "The binary gap of five (0101)" should "be one" in {
-    val n = 5
-    val expected = 1
-    val binaryGap = NumericChallenges.findBinaryGap(n)
-    assert(binaryGap == expected)
+class NumericChallengesSpec extends Specification {
+
+  "The binary gap function" should {
+    "return one as the binary gap of five (0101)" in {
+      val n = 5
+      val expected = 1
+      val binaryGap = NumericChallenges.findBinaryGap(n)
+      binaryGap mustEqual expected
+    }
+
+    "return one as the binary gap of twenty (10100)" in {
+      val n = 20
+      val expected = 1
+      val binaryGap = NumericChallenges.findBinaryGap(n)
+      binaryGap mustEqual expected
+    }
+
+    "return zero as the binary gap of fifteen (11111)" in {
+      val n = 15
+      val expected = 0
+      val binaryGap = NumericChallenges.findBinaryGap(n)
+      binaryGap mustEqual expected
+    }
   }
 
-  "The binary gap of twenty (10100)" should "be one" in {
-    val n = 20
-    val expected = 1
-    val binaryGap = NumericChallenges.findBinaryGap(n)
-    assert(binaryGap == expected)
-  }
+  "To determine the number of jumps of a frog, the function" should {
+    "return 3 jumps to go from [10 to 85] given a jump distance of 30" in {
+      val x = 10
+      val y = 85
+      val d = 30
+      val expected = 3
+      val jumps = NumericChallenges.frogJumps(x, y, d)
+      jumps mustEqual expected
+    }
 
-  "The binary gap of fifteen (11111)" should "be one" in {
-    val n = 15
-    val expected = 0
-    val binaryGap = NumericChallenges.findBinaryGap(n)
-    assert(binaryGap == expected)
-  }
-
-  "The number of jumps to go from  [10 to 85] given a jump distance of 30" should "be 3" in {
-    val x = 10
-    val y = 85
-    val d = 30
-    val expected = 3
-    val jumps = NumericChallenges.frogJumps(x, y, d)
-    assert(jumps == expected)
-  }
-
-  "The number of jumps to go from  [3 to 999111321] given a jump distance of 7" should "be 142730189" in {
-    val x = 3
-    val y = 999111321
-    val d = 7
-    val expected = 142730189
-    val jumps = NumericChallenges.frogJumps(x, y, d)
-    assert(jumps == expected)
+    "return 142.730.189 jumps to go from [3 to 999111321] given a jump distance of 7" in {
+      val x = 3
+      val y = 999111321
+      val d = 7
+      val expected = 142730189
+      val jumps = NumericChallenges.frogJumps(x, y, d)
+      jumps mustEqual expected
+    }
   }
 }
