084d1f5b746bba92a52a3cf4fca240a7300a476d
Geektimus <alexander.kno+github@gmail.com>
2019-03-05 17:25:09 +0100

    Add scala style plugin and Rewrite functions to follow it's conventions

--- src/main/scala/com/codingmaniacs/qualified/NumericChallenges.scala
+++ src/main/scala/com/codingmaniacs/qualified/NumericChallenges.scala
@@ -8,13 +8,19 @@ object NumericChallenges {
     * @return Ordinal representation of i
     */
   def fromNumber(i: Int): String = {
-    i.toString match {
-      case "0" => "0"
-      case n if n.endsWith("11") || n.endsWith("12") || n.endsWith("13") => i + "th"
-      case n if n.endsWith("1") => i + "st"
-      case n if n.endsWith("2") => i + "nd"
-      case n if n.endsWith("3") => i + "rd"
-      case _ => i + "th"
+    def returnPrefixForValue(value: Int): String = {
+
+      val lastTwoDigits = value % 100;
+      val lastDigit = lastTwoDigits % 10;
+
+      (lastDigit, lastTwoDigits) match {
+        case (_, ltd) if ltd >= 11 && ltd <=13 => "th"
+        case (1, _) => "st"
+        case (2, _) => "nd"
+        case (3, _) => "rd"
+        case _ => "th"
+      }
     }
+    if (i == 0) { "0" } else {i + returnPrefixForValue(i)}
   }
 }
