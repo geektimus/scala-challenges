8787d880e959448263879760af3204dc10aa1b58
Geektimus <alexander.kno+github@gmail.com>
2019-03-06 15:27:26 +0100

    Add scala coverage plugin and Refactor other class for scalastyle

--- src/main/scala/com/codingmaniacs/codility/CollectionChallenges.scala
+++ src/main/scala/com/codingmaniacs/codility/CollectionChallenges.scala
@@ -10,21 +10,21 @@ object CollectionChallenges {
   def findMissingInt(data: Array[Int]): Int = {
     data match {
       case _ if data == null || data.length == 0 => 1
-      case d => val arr = d.filter(n => n > 0).distinct.sorted
+      case d =>
+        val arr = d.filter(n => n > 0).distinct.sorted
+        arr match {
+          case empty if empty.isEmpty || arr(0) > 1 => 1
+          case a =>
+            val res = a
+              .zipWithIndex
+              .map(el => (el._1, el._2 + 1))
+              .dropWhile(el => el._1 - el._2 == 0)
 
-        if (arr.isEmpty || arr(0) > 1) {
-          return 1
-        }
-
-        val res = arr
-          .zipWithIndex
-          .map(el => (el._1, el._2 + 1))
-          .dropWhile(el => el._1 - el._2 == 0)
-
-        if (res.isEmpty) {
-          arr.last + 1
-        } else {
-          res.head._2
+            if (res.isEmpty) {
+              arr.last + 1
+            } else {
+              res.head._2
+            }
         }
     }
   }
@@ -47,13 +47,15 @@ object CollectionChallenges {
     */
   def findTapeEquilibrium(ns: Array[Int]): Int = {
 
+    val maxDefaultTapeEq = 999999999
+
     val absMin: (Int, Int) => Int = (x: Int, y: Int) => {
       Math.abs(x - y)
     }
 
     var start = ns.head
     var end = ns.tail.sum
-    var min = if (ns.length == 2) absMin(ns.head, ns.tail.head) else 999999999
+    var min = if (ns.length == 2) absMin(ns.head, ns.tail.head) else maxDefaultTapeEq
 
     for (i <- 1 to ns.length - 2) {
       val total = absMin(start, end)
