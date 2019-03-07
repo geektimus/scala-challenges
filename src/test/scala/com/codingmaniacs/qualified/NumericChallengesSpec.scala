097f7257137cfce7d5f8feed19b3521b3d6a34e5
Geektimus <alexander.kno+github@gmail.com>
2019-03-08 00:55:00 +0100

    Refactor findMissingInt challenge to be more 'functional'

--- src/test/scala/com/codingmaniacs/qualified/NumericChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/qualified/NumericChallengesSpec.scala
@@ -9,31 +9,34 @@ class NumericChallengesSpec extends Specification {
       res mustEqual "0"
     }
     "handle the basic number (1,2,3,4)" in {
-      List((1, "1st"), (2, "2nd"), (3, "3rd"), (4, "4th"))
-        .foreach(el => {
-          val res = NumericChallenges.fromNumber(el._1)
-          res mustEqual el._2
-        })
-      // TODO Find a way to test multiple things without the need for this 1 = 1 statement to force this to compile
-      "1" mustEqual "1"
+      // scalastyle:off magic.number
+      val numbers = List(1, 2, 3, 4)
+      // scalastyle:on magic.number
+
+      val expected = List("1st", "2nd", "3rd", "4th")
+      val ordinalNumbers = numbers.map(NumericChallenges.fromNumber)
+
+      ordinalNumbers mustEqual expected
     }
     "handle the basic number (22,33,44,55)" in {
-      List((22, "22nd"), (33, "33rd"), (44, "44th"), (55, "55th"))
-        .foreach(el => {
-          val res = NumericChallenges.fromNumber(el._1)
-          res mustEqual el._2
-        })
-      // TODO Find a way to test multiple things without the need for this 1 = 1 statement to force this to compile
-      "1" mustEqual "1"
+      // scalastyle:off magic.number
+      val numbers = List(22, 33, 44, 55)
+      // scalastyle:on magic.number
+
+      val expected = List("22nd", "33rd", "44th", "55th")
+      val ordinalNumbers = numbers.map(NumericChallenges.fromNumber)
+
+      ordinalNumbers mustEqual expected
     }
     "handle the special cases (11,12,13)" in {
-      List((11, "11th"), (12, "12th"), (13, "13th"))
-        .foreach(el => {
-          val res = NumericChallenges.fromNumber(el._1)
-          res mustEqual el._2
-        })
-      // TODO Find a way to test multiple things without the need for this 1 = 1 statement to force this to compile
-      "1" mustEqual "1"
+      // scalastyle:off magic.number
+      val numbers = List(11, 12, 13)
+      // scalastyle:on magic.number
+
+      val expected = List("11th", "12th", "13th")
+      val ordinalNumbers = numbers.map(NumericChallenges.fromNumber)
+
+      ordinalNumbers mustEqual expected
     }
   }
 }
