6c6ed3817ba0fcfa9cf1b89ab75b1155ab15c9fb
Geektimus <alexander.kno+github@gmail.com>
2019-03-07 21:00:12 +0100

    Disable magic number check for some tests and simplify forNumber method

--- src/test/scala/com/codingmaniacs/codility/CollectionChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/codility/CollectionChallengesSpec.scala
@@ -27,14 +27,18 @@ class CollectionChallengesSpec extends Specification {
     }
 
     "show that [5] is a missing integer on [1, 3, 6, 4, 1, 2]" in {
+      // scalastyle:off magic.number
       val numbers = Array(1, 3, 6, 4, 1, 2)
+      // scalastyle:on magic.number
       val expected = 5
       val missingInteger = CollectionChallenges.findMissingInt(numbers)
       missingInteger mustEqual expected
     }
 
     "show that [3] is a missing integer on [4, 1, 5, 6, 2]" in {
+      // scalastyle:off magic.number
       val numbers = Array(4, 1, 5, 6, 2)
+      // scalastyle:on magic.number
       val expected = 3
       val missingInteger = CollectionChallenges.findMissingInt(numbers)
       missingInteger mustEqual expected
@@ -48,7 +52,9 @@ class CollectionChallengesSpec extends Specification {
     }
 
     "show that [1] is a missing integer on [-1,-3]" in {
+      // scalastyle:off magic.number
       val numbers = Array(-1, -3)
+      // scalastyle:on magic.number
       val expected = 1
       val missingInteger = CollectionChallenges.findMissingInt(numbers)
       missingInteger mustEqual expected
@@ -71,21 +77,27 @@ class CollectionChallengesSpec extends Specification {
     }
 
     "return 3 as the unpaired element on [1,1,3,4,2,2,4]" in {
+      // scalastyle:off magic.number
       val numbers = Array(1, 1, 3, 4, 2, 2, 4)
+      // scalastyle:on magic.number
       val expected = 3
       val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
       unpairedNumber mustEqual expected
     }
 
     "return 5 as the unpaired element on [4,4,2,2,5,1,1,1,1,4,4]" in {
+      // scalastyle:off magic.number
       val numbers = Array(4, 4, 2, 2, 5, 1, 1, 1, 1, 4, 4)
+      // scalastyle:on magic.number
       val expected = 5
       val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
       unpairedNumber mustEqual expected
     }
 
     "return 5 as the unpaired element on [4,3,2,1,5,1,2,3,4]" in {
+      // scalastyle:off magic.number
       val numbers = Array(4, 3, 2, 1, 5, 1, 2, 3, 4)
+      // scalastyle:on magic.number
       val expected = 5
       val unpairedNumber = CollectionChallenges.findUnpairedElement(numbers)
       unpairedNumber mustEqual expected
@@ -101,28 +113,36 @@ class CollectionChallengesSpec extends Specification {
     }
 
     "return 4 as the equilibrium on the tape [2,6]" in {
+      // scalastyle:off magic.number
       val numbers = Array(2, 6)
+      // scalastyle:on magic.number
       val expected = 4
       val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
       equilibrium mustEqual expected
     }
 
     "return 0 as the equilibrium on the tape [55,10,45]" in {
+      // scalastyle:off magic.number
       val numbers = Array(55, 10, 45)
+      // scalastyle:on magic.number
       val expected = 0
       val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
       equilibrium mustEqual expected
     }
 
     "return 1 as the equilibrium on the tape [3,1,2,4,3]" in {
+      // scalastyle:off magic.number
       val numbers = Array(3, 1, 2, 4, 3)
+      // scalastyle:on magic.number
       val expected = 1
       val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
       equilibrium mustEqual expected
     }
 
     "return 2000 as the equilibrium on the tape [-1000,1000]" in {
+      // scalastyle:off magic.number
       val numbers = Array(-1000, 1000)
+      // scalastyle:on magic.number
       val expected = 2000
       val equilibrium = CollectionChallenges.findTapeEquilibrium(numbers)
       equilibrium mustEqual expected
