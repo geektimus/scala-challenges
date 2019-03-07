26ff2c09fc2d2881f18e0c2f0c6cb974e8e1c3ab
Geektimus <alexander.kno+github@gmail.com>
2019-03-07 21:07:54 +0100

    Update variable names after codacy validation

--- src/test/scala/com/codingmaniacs/others/NumericChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/others/NumericChallengesSpec.scala
@@ -4,36 +4,36 @@ import org.specs2.mutable.Specification
 
 class NumericChallengesSpec extends Specification {
 
-  val NORMAL_VALUE = 1
-  val FIZZ_VALUE = 6
-  val BUZZ_VALUE = 10
-  val FIZZ_BUZZ_VALUE = 30
+  val normalValue = 1
+  val fizzValue = 6
+  val buzzValue = 10
+  val fizzBuzzValue = 30
 
   "The fizz buzz method" should {
 
     "return the number for numbers that are not multiples of 3 and/or 5" in {
-      val s = Seq(NORMAL_VALUE)
+      val s = Seq(normalValue)
       val expected = Seq("1")
       val reverse = NumericChallenges.fizzBuzz(s.toList)
       reverse mustEqual expected
     }
 
     "return Fizz for multiples of 3" in {
-      val s = Seq(FIZZ_VALUE)
+      val s = Seq(fizzValue)
       val expected = Seq("Fizz")
       val reverse = NumericChallenges.fizzBuzz(s)
       reverse mustEqual expected
     }
 
     "return Buzz for multiples of 5" in {
-      val s = Seq(BUZZ_VALUE)
+      val s = Seq(buzzValue)
       val expected = Seq("Buzz")
       val reverse = NumericChallenges.fizzBuzz(s)
       reverse mustEqual expected
     }
 
     "return FizzBuzz for multiples of 3 and 5" in {
-      val s = Seq(FIZZ_BUZZ_VALUE)
+      val s = Seq(fizzBuzzValue)
       val expected = Seq("FizzBuzz")
       val reverse = NumericChallenges.fizzBuzz(s)
       reverse mustEqual expected
