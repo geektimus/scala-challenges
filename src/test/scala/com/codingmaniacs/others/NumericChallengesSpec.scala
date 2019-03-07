0e7f81d150e2dd9d9ed6f227c6617bdce88140d2
Geektimus <alexander.kno+github@gmail.com>
2019-03-07 20:32:49 +0100

    Update to remove the usage of Magic Numbers (scalastyle)

--- src/test/scala/com/codingmaniacs/others/NumericChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/others/NumericChallengesSpec.scala
@@ -3,32 +3,38 @@ package com.codingmaniacs.others
 import org.specs2.mutable.Specification
 
 class NumericChallengesSpec extends Specification {
+
+  val NORMAL_VALUE = 1
+  val FIZZ_VALUE = 6
+  val BUZZ_VALUE = 10
+  val FIZZ_BUZZ_VALUE = 30
+
   "The fizz buzz method" should {
 
     "return the number for numbers that are not multiples of 3 and/or 5" in {
-      val s = Seq(1, 2, 4, 7, 8)
-      val expected = Seq("1", "2", "4", "7", "8")
-      val reverse = NumericChallenges.fizzBuzz(s)
+      val s = Seq(NORMAL_VALUE)
+      val expected = Seq("1")
+      val reverse = NumericChallenges.fizzBuzz(s.toList)
       reverse mustEqual expected
     }
 
     "return Fizz for multiples of 3" in {
-      val s = Seq(3, 6, 9, 12)
-      val expected = Seq("Fizz", "Fizz", "Fizz", "Fizz")
+      val s = Seq(FIZZ_VALUE)
+      val expected = Seq("Fizz")
       val reverse = NumericChallenges.fizzBuzz(s)
       reverse mustEqual expected
     }
 
     "return Buzz for multiples of 5" in {
-      val s = Seq(5, 10, 20, 25)
-      val expected = Seq("Buzz", "Buzz", "Buzz", "Buzz")
+      val s = Seq(BUZZ_VALUE)
+      val expected = Seq("Buzz")
       val reverse = NumericChallenges.fizzBuzz(s)
       reverse mustEqual expected
     }
 
     "return FizzBuzz for multiples of 3 and 5" in {
-      val s = Seq(0, 15, 30, 45, 60)
-      val expected = Seq("FizzBuzz", "FizzBuzz", "FizzBuzz", "FizzBuzz", "FizzBuzz")
+      val s = Seq(FIZZ_BUZZ_VALUE)
+      val expected = Seq("FizzBuzz")
       val reverse = NumericChallenges.fizzBuzz(s)
       reverse mustEqual expected
     }
