829af16f0e2d8aab8a6c0ac9fc5a9d404a65cf79
Geektimus <alexander.kno+github@gmail.com>
2019-03-07 21:00:12 +0100

    Disable magic number check for some tests and simplify forNumber method

--- src/main/scala/com/codingmaniacs/qualified/NumericChallenges.scala
+++ src/main/scala/com/codingmaniacs/qualified/NumericChallenges.scala
@@ -4,23 +4,26 @@ object NumericChallenges {
   /**
     * Returns the string representation of a given number, providing the ordinal representation of the given number
     *
-    * @param i number
+    * @param value number
     * @return Ordinal representation of i
     */
-  def fromNumber(i: Int): String = {
-    def returnPrefixForValue(value: Int): String = {
-
-      val lastTwoDigits = value % 100;
-      val lastDigit = lastTwoDigits % 10;
-
-      (lastDigit, lastTwoDigits) match {
-        case (_, ltd) if ltd >= 11 && ltd <=13 => "th"
-        case (1, _) => "st"
-        case (2, _) => "nd"
-        case (3, _) => "rd"
-        case _ => "th"
-      }
+  def fromNumber(value: Int): String = {
+    value match {
+      case 0 => "0"
+      case _ => s"$value${returnPrefixForValue(value)}"
     }
-    if (i == 0) { "0" } else {i + returnPrefixForValue(i)}
+  }
+
+  def returnPrefixForValue(value: Int): String = {
+    val lastTwoDigits = value % 100
+    val lastDigit = lastTwoDigits % 10
+
+    (lastDigit, lastTwoDigits) match {
+      case (_, ltd) if ltd >= 11 && ltd <=13 => "th"
+      case (1, _) => "st"
+      case (2, _) => "nd"
+      case (3, _) => "rd"
+      case _ => "th"
+    }
   }
 }
