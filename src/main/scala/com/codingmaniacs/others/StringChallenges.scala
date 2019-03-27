3f3245ffae0a5924c200c071e7c00607dc4c9fae
Geektimus <alexander.kno+github@gmail.com>
2019-03-27 17:54:21 +0100

    Add the solution for the word count problem

--- src/main/scala/com/codingmaniacs/others/StringChallenges.scala
+++ src/main/scala/com/codingmaniacs/others/StringChallenges.scala
@@ -1,10 +1,11 @@
 package com.codingmaniacs.others
 
+import scala.annotation.tailrec
 import scala.util.matching.Regex
 
 object StringChallenges {
-
   val extractionPattern: Regex = "(.*)([a-zA-Z\\s])$".r
+
 
   /**
     * Given a string it returns the reverse of the string using foldRight
@@ -14,5 +15,39 @@ object StringChallenges {
     */
   def flip(str: String): String = {
     str.foldRight("") ((el, acc) => acc + el)
+  }
+
+  def wordCountWithSplit(str: String): Int = {
+    str
+      .trim
+      .split(" ")
+      .map(w => w.trim)
+      .count(w => !w.equals(""))
+  }
+
+  def wordCountWithRec(str: String): Int = {
+
+    @tailrec
+    def wordCountRec(charSeq: List[Char], spaces: Int): Int = {
+      charSeq match {
+        case Nil => spaces + 1
+        case h :: th :: tail if h == 32 && th == 32 => {
+          wordCountRec(th :: tail, spaces)
+        }
+        case h :: th :: tail if h == 32 && th != 32 => {
+          wordCountRec(tail, spaces + 1)
+        }
+        case _ :: tail => {
+          wordCountRec(tail, spaces)
+        }
+      }
+    }
+    str match {
+      case "" => 0
+      case _ =>
+        val trimmed = str.trim
+        val sequence = trimmed.toList
+        wordCountRec(sequence, 0)
+    }
   }
 }
--- src/test/scala/com/codingmaniacs/others/StringChallengesSpec.scala
+++ src/test/scala/com/codingmaniacs/others/StringChallengesSpec.scala
@@ -39,4 +39,90 @@ class StringChallengesSpec extends Specification {
       reverse mustEqual expected
     }
   }
+
+  "The string analyzer" should {
+    "count the number of words on an empty string as zero (0)" in {
+      val s = ""
+      val expected = 0
+      val count = StringChallenges.wordCountWithSplit(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a simple word as one (1)" in {
+      val s = "simple"
+      val expected = 1
+      val count = StringChallenges.wordCountWithSplit(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase" in {
+      val s = "simple1 simple2 simple3"
+      val expected = 3
+      val count = StringChallenges.wordCountWithSplit(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase with spaces at the start" in {
+      val s = "       simple1 simple2 simple3"
+      val expected = 3
+      val count = StringChallenges.wordCountWithSplit(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase with spaces at the end" in {
+      val s = "simple1 simple2 simple3      "
+      val expected = 3
+      val count = StringChallenges.wordCountWithSplit(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase with spaces all over the place" in {
+      val s = "   simple1    simple2  simple3      "
+      val expected = 3
+      val count = StringChallenges.wordCountWithSplit(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on an empty string as zero (0)" in {
+      val s = ""
+      val expected = 0
+      val count = StringChallenges.wordCountWithRec(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a simple word as one (1)" in {
+      val s = "simple"
+      val expected = 1
+      val count = StringChallenges.wordCountWithRec(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase" in {
+      val s = "simple1 simple2 simple3"
+      val expected = 3
+      val count = StringChallenges.wordCountWithRec(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase with spaces at the start" in {
+      val s = "       simple1 simple2 simple3"
+      val expected = 3
+      val count = StringChallenges.wordCountWithRec(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase with spaces at the end" in {
+      val s = "simple1 simple2 simple3      "
+      val expected = 3
+      val count = StringChallenges.wordCountWithRec(s)
+      count mustEqual expected
+    }
+
+    "count the number of words on a phrase with spaces all over the place" in {
+      val s = "   simple1    simple2  simple3      "
+      val expected = 3
+      val count = StringChallenges.wordCountWithRec(s)
+      count mustEqual expected
+    }
+  }
 }
