0f1bddd7eaebbd7480bcaea6d64a47b163a327f4
Geektimus <alexander.kno+github@gmail.com>
2019-04-09 00:36:42 +0200

    Extract the recursive function to its own declaration

--- src/main/scala/com/codingmaniacs/others/StringChallenges.scala
+++ src/main/scala/com/codingmaniacs/others/StringChallenges.scala
@@ -6,7 +6,6 @@ import scala.util.matching.Regex
 object StringChallenges {
   val extractionPattern: Regex = "(.*)([a-zA-Z\\s])$".r
 
-
   /**
     * Given a string it returns the reverse of the string using foldRight
     *
@@ -38,27 +37,25 @@ object StringChallenges {
     * @return Number of words found on the string using recursion
     */
   def wordCountWithRec(str: String): Int = {
-
-    @tailrec
-    def wordCountRec(charSeq: List[Char], spaces: Int): Int = {
-      charSeq match {
-        case Nil => spaces + 1
-        case h :: th :: tail if h == 32 && th == 32 =>
-          wordCountRec(th :: tail, spaces)
-        case h :: th :: tail if h == 32 && th != 32 =>
-          wordCountRec(tail, spaces + 1)
-        case _ :: tail => {
-          wordCountRec(tail, spaces)
-        }
-      }
-    }
-
     str match {
       case "" => 0
       case _ =>
         val trimmed = str.trim
         val sequence = trimmed.toList
         wordCountRec(sequence, 0)
+    }
+  }
+
+  @tailrec
+  def wordCountRec(charSeq: List[Char], spaces: Int): Int = {
+    charSeq match {
+      case Nil => spaces + 1
+      case h :: th :: tail if h == 32 && th == 32 =>
+        wordCountRec(th :: tail, spaces)
+      case h :: th :: tail if h == 32 && th != 32 =>
+        wordCountRec(tail, spaces + 1)
+      case _ :: tail =>
+        wordCountRec(tail, spaces)
     }
   }
 }
