24036d945157003928a2a55ff74f9a2aea2b2f8c
Geektimus <alexander.kno+github@gmail.com>
2019-04-09 12:36:50 +0200

    Add right-aligned staircase challenge

--- src/main/scala/com/codingmaniacs/hackerrank/CollectionChallenges.scala
+++ src/main/scala/com/codingmaniacs/hackerrank/CollectionChallenges.scala
@@ -7,7 +7,7 @@ object CollectionChallenges {
   def findNumber(arr: Array[Int], k: Int): String = {
 
     @tailrec
-    def findK(ls: List[Int], n: Int) : String = {
+    def findK(ls: List[Int], n: Int): String = {
       ls match {
         case Nil => "NO"
         case h :: _ if h == n => "YES"
@@ -18,8 +18,8 @@ object CollectionChallenges {
     findK(arr.toList, k)
   }
 
-  def findNumbers(start: Int, end: Int, predicate: Int => Boolean) : Array[Int] = {
-    def findNumbersRec(res: List[Int], rem: List[Int], predicate: Int => Boolean) : Array[Int] = {
+  def findNumbers(start: Int, end: Int, predicate: Int => Boolean): Array[Int] = {
+    def findNumbersRec(res: List[Int], rem: List[Int], predicate: Int => Boolean): Array[Int] = {
       rem match {
         case Nil => res.toArray
         case h :: tail if predicate(h) => findNumbersRec(res ::: List(h), tail, predicate)
@@ -31,9 +31,25 @@ object CollectionChallenges {
   }
 
   def oddNumbers(start: Int, end: Int): Array[Int] = {
-    def oddPredicate(h: Int) : Boolean = {
+    def oddPredicate(h: Int): Boolean = {
       h % 2 != 0
     }
+
     findNumbers(start, end, oddPredicate)
+  }
+
+  def generateStaircase(size: Int): List[List[String]] = {
+
+    val generateLevelString = (d: Int, l: Int) => (" " * (d - l)) + "#" * l
+
+    @tailrec
+    def generateLevel(dimensions: Int, n: Int, staircase: List[List[String]]): List[List[String]] = {
+      n match {
+        case 0 => staircase
+        case l => generateLevel(dimensions, n - 1, List(generateLevelString(dimensions, l)) :: staircase)
+      }
+    }
+
+    generateLevel(size, size, List())
   }
 }
